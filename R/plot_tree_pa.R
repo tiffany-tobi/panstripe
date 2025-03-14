8401a0f582e9baa31024aae51a8834db8dadab88
tiffany-tobi <tiffanyttobi@icloud.com>
2025-03-14 10:04:25 +0000

    Update plot_tree_pa.R for myself

--- R/plot_tree_pa.R
+++ R/plot_tree_pa.R
@@ -97,6 +97,7 @@ plot_tree_pa <- function(tree, pa, genes=colnames(pa),
     ggplot2::scale_y_continuous(limits = c(0, ntips+1)) +
     ggplot2::theme_bw(base_size = text_size) +
     ggplot2::theme(axis.title.y=ggplot2::element_blank(),
+          axis.text.x=ggplot2::element_text(angle = 45, hjust = 1),
           axis.text.y=ggplot2::element_blank(),
           axis.ticks.y=ggplot2::element_blank(),
           axis.ticks.x=ggplot2::element_blank()) +
