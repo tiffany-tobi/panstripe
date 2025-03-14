051d6102bcc23b39225b0b3e0ead38c5d1999484
tiffany-tobi <tiffanyttobi@icloud.com>
2025-03-14 10:38:03 +0000

    Update plot_tree_pa.R
    another try

--- R/plot_tree_pa.R
+++ R/plot_tree_pa.R
@@ -98,6 +98,7 @@ plot_tree_pa <- function(tree, pa, genes=colnames(pa),
     ggplot2::theme_bw(base_size = text_size) +
     ggplot2::theme(axis.title.y=ggplot2::element_blank(),
           axis.text.y=ggplot2::element_blank(),
+          axis.text.x=ggplot2::element_text(angle = 45),
           axis.ticks.y=ggplot2::element_blank(),
           axis.ticks.x=ggplot2::element_blank()) +
     ggplot2::theme(panel.background = ggplot2::element_blank(),
@@ -112,7 +113,7 @@ plot_tree_pa <- function(tree, pa, genes=colnames(pa),
   
   if (!label_genes){
     gg <- gg +
-      ggplot2::theme(axis.text.x=ggplot2::element_text(angle = 45, hjust = 1),
+      ggplot2::theme(axis.text.x=ggplot2::element_blank(),
                      axis.ticks.x=ggplot2::element_blank(),
                      panel.grid.major.x = ggplot2::element_blank(),
                      panel.grid.minor.x = ggplot2::element_blank()) 
